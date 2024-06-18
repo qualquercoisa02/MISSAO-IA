965d83be26b55a9b5ab6f072c1a536a00f816a1f
Heloísa <135342007+qualquercoisa02@users.noreply.github.com>
2024-06-18 11:15:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const pergunta = [
     {
-        enunciado: "Qual o impacto da Inteligência Artificial na sua vida?",
+        enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que pode responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o sseu primeiro pensamento?",
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
@@ -28,6 +28,24 @@ let atual = 0;
 let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = pergunta[atual];
-    caixaPergunta.textContent = perguntaAtual.enunciado;
-}
-mostraPergunta();
+    caixaPergunta.textContent = perguntaAtual.enunciado;}
+    mostraPergunta ();
+    mostraAlternativa();
+function mostraAlternativa() {
+
+    enunciado : "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que pode responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o sseu primeiro pensamento?",
+
+    alternativa= [
+        "Isso é assustador!",
+        "Isso é maravilhoso!"
+    ]
+perguntaAtual.alternativa
+
+function mostraAlternativa() {
+    for (const alternativa of perguntaAtual.alternativa) {
+        const botaoAlternativa =
+    document.createElement ("button");
+botaoAlternativa.textContent = alternativa;    
+caixaAlternativa.appendChild(botaoAlternativa);
+
+    }}}
