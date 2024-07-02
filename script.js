8bfc0cb70fa533d8538e0245086042e1f724b98e
Heloísa <135342007+qualquercoisa02@users.noreply.github.com>
2024-07-02 11:34:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,50 +2,49 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 
 const caixaPergunta = document.querySelector(".caixa-pergunta");
 
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
 
 const caixaResultado = document.querySelector(".caixa-resultado");
 
 const textoResultado = document.querySelector(".texto-resultado");
 
+
 const pergunta = [
-    {
-        enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que pode responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o sseu primeiro pensamento?",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ],
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ],
-    },
+{
+enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?",
+alternativas: [
+"Isso é assustador!",
+"Isso é maravilhoso!"
+],
+},
+{
+enunciado: "Pergunta 2",
+alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+],
+},
 ];
 let atual = 0;
 let perguntaAtual;
-function mostraPergunta() {
-    perguntaAtual = pergunta[atual];
-    caixaPergunta.textContent = perguntaAtual.enunciado;}
-    mostraPergunta ();
-    mostraAlternativa();
-function mostraAlternativa() {
-
-    enunciado : "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que pode responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o sseu primeiro pensamento?",
-
-    alternativa= [
-        "Isso é assustador!",
-        "Isso é maravilhoso!"
-    ]
-perguntaAtual.alternativa
+function mostrapergunta() {
+perguntaAtual = pergunta[atual];
+caixaPergunta.textContent = perguntaAtual.enunciado;
+mostraAlternativas();
+}
 
 function mostraAlternativa() {
-    for (const alternativa of perguntaAtual.alternativa) {
-        const botaoAlternativa =
-    document.createElement ("button");
-botaoAlternativa.textContent = alternativa;    
-caixaAlternativa.appendChild(botaoAlternativa);
+for (const alternativa of perguntaAtual.alternativas) {
+const botaoAlternativas = document. createElement("button");
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click",
+function () {
+atual++;
+mostraPergunta();
+});
+caixaAlternativas.appendChild(botaoAlternativas);
+}
+}
 
-    }}}
+
+mostrapergunta();
