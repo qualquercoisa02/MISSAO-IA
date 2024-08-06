f8f00ad6785dbba8f3adcca2c791a17f0e42e70e
Heloísa <135342007+qualquercoisa02@users.noreply.github.com>
2024-08-06 10:36:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,19 +32,12 @@ perguntaAtual = pergunta[atual];
 caixaPergunta.textContent = perguntaAtual.enunciado;
 mostraAlternativas();
 }
-
-function mostraAlternativa() {
-for (const alternativa of perguntaAtual.alternativas) {
-const botaoAlternativas = document. createElement("button");
-botaoAlternativas.textContent = alternativa.texto;
-botaoAlternativas.addEventListener("click",
-function () {
-atual++;
-mostraPergunta();
-});
+function mostraAlternativas() {
+for (const alternativa of perguntaAtual.alternativas)
+{
+const botaoalternativas = document.createElement("button");
+botaoAlternativas.textContent = alternativa;
 caixaAlternativas.appendChild(botaoAlternativas);
-}
-}
 
-
+}}
 mostrapergunta();
